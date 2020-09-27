209480d769f0bf1be1cf532a97ff9e0f60b29945
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-09-26 19:03:41 -0700

    PrelogV1.cpp
    An archive of the first version of the pre-login C++ script.

---
+++ SWave_Starter/OldVersions/Prelog_CPP/PrelogV1.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+// This script allows the program to play audio at startup levels. The program must have root access to do so, and is not recommended at the moment.
+int main()
+{
+    std::cout << "Testing...\nSWave_Starter";
+}
