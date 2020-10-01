665f089a02d7a06e1a57c4729eea147674dbcc69
Sean P. Myrick V19.0.4.2 <65933340+seanpm2001@users.noreply.github.com>
2020-10-01 12:30:13 -0700

    Enable_Startup_Sound_Ubuntu.sh
    The startup sound enabling script for Ubuntu.

---
+++ Repair/Linux/Ubuntu/Enable_Startup_Sound_Ubuntu.sh
@@ -0,0 +1,3 @@
+# Ubuntu 11.10
+sudo cp /usr/share/sounds/ubuntu/stereo/* /usr/share/sounds/ # This script will re-enable the Ubuntu 11.10 startup sound
+# Also works with: Ubuntu 12.04-20.04 (not yet confirmed)
